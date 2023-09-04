eb3fd8bfbc7a888206c3cca01c45bc6751ed50d2
Hsiao Yao <tomtogo1260@gmail.com>
2023-09-05 03:34:31 +0800

    fix

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 import * as THREE from 'three'
+import textureViridis from './textures/cm_viridis.png'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
 
 const sizes = {
@@ -6,9 +7,12 @@ const sizes = {
     height: window.innerHeight
 }
 
-const cmTexture = new THREE.TextureLoader().load('cm_viridis.png')
-const tifTexture = new THREE.TextureLoader().load('00000.png')
+const cmTexture = new THREE.TextureLoader().load(textureViridis)
+const tifTexture = new THREE.TextureLoader().load('00000.png', tick)
 const scene = new THREE.Scene()
+
+tifTexture.minFilter = THREE.LinearFilter
+tifTexture.magFilter = THREE.LinearFilter
 
 const geometry = new THREE.PlaneGeometry(2, 2, 1, 1)
 const material = new THREE.ShaderMaterial({
@@ -41,7 +45,13 @@ const material = new THREE.ShaderMaterial({
     void main() {
       float r = screenAspect / volumeAspect;
       float aspect = r;
-      vec2 uv = vec2((vUv.x - 0.5) * aspect, (vUv.y - 0.5)) + vec2(0.5);
+
+      vec2 vUv_;
+      vUv_ = vUv;
+      // vUv_.x = 0.2 * vUv.x + 0.4;
+      // vUv_.y = 0.2 * vUv.y + 0.4;
+
+      vec2 uv = vec2((vUv_.x - 0.5) * aspect, (vUv_.y - 0.5)) + vec2(0.5);
       if ( uv.x < 0.0 || uv.x > 1.0 || uv.y < 0.0 || uv.y > 1.0 ) return;
 
       float intensity = texture2D(utifTexture, uv).r;
@@ -55,17 +65,17 @@ scene.add(new THREE.Mesh(geometry, material))
 const canvas = document.querySelector('canvas.webgl')
 
 window.addEventListener('resize', () => {
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
+  // Update sizes
+  sizes.width = window.innerWidth
+  sizes.height = window.innerHeight
 
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
+  // Update camera
+  camera.aspect = sizes.width / sizes.height
+  camera.updateProjectionMatrix()
 
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+  // Update renderer
+  renderer.setSize(sizes.width, sizes.height)
+  renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
@@ -75,20 +85,17 @@ scene.add(camera)
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
 
-const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
-})
+const renderer = new THREE.WebGLRenderer({ canvas })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
-renderer.render(scene, camera)
-
-
-const tick = () => {
-  controls.update()
+function tick() {
   renderer.render(scene, camera)
 
-  window.requestAnimationFrame(tick)
+  // const imgData = renderer.domElement.toDataURL('image/png')
+  // const link = document.createElement('a')
+  // link.href = imgData
+  // link.download = 'example'
+  // link.click()
 }
 
-tick()
